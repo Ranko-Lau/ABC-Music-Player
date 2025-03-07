253b813815192e8348322f8052af1be3b8531576
明月心 <rankolau@gmail.com>
2025-03-07 15:21:25 +0800

    Basic work on g4, add spec. and a helper in LexerTest

---
+++ src/abc/parser/ABCmusicParser.java
@@ -0,0 +1,153 @@
+// Generated from ABCmusic.g4 by ANTLR 4.5.1
+
+package abc.parser;
+// Do not edit this .java file! Edit the .g4 file and re-run Antlr.
+
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class ABCmusicParser extends Parser {
+  static { RuntimeMetaData.checkVersion("4.5.1", RuntimeMetaData.VERSION); }
+
+  protected static final DFA[] _decisionToDFA;
+  protected static final PredictionContextCache _sharedContextCache =
+    new PredictionContextCache();
+  public static final int
+    T__0=1, TEXT=2, DIGIT=3, EOL=4;
+  public static final int
+    RULE_abc_tune = 0;
+  public static final String[] ruleNames = {
+    "abc_tune"
+  };
+
+  private static final String[] _LITERAL_NAMES = {
+    null, "'+'"
+  };
+  private static final String[] _SYMBOLIC_NAMES = {
+    null, null, "TEXT", "DIGIT", "EOL"
+  };
+  public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+  /**
+   * @deprecated Use {@link #VOCABULARY} instead.
+   */
+  @Deprecated
+  public static final String[] tokenNames;
+  static {
+    tokenNames = new String[_SYMBOLIC_NAMES.length];
+    for (int i = 0; i < tokenNames.length; i++) {
+      tokenNames[i] = VOCABULARY.getLiteralName(i);
+      if (tokenNames[i] == null) {
+        tokenNames[i] = VOCABULARY.getSymbolicName(i);
+      }
+
+      if (tokenNames[i] == null) {
+        tokenNames[i] = "<INVALID>";
+      }
+    }
+  }
+
+  @Override
+  @Deprecated
+  public String[] getTokenNames() {
+    return tokenNames;
+  }
+
+  @Override
+
+  public Vocabulary getVocabulary() {
+    return VOCABULARY;
+  }
+
+  @Override
+  public String getGrammarFileName() { return "ABCmusic.g4"; }
+
+  @Override
+  public String[] getRuleNames() { return ruleNames; }
+
+  @Override
+  public String getSerializedATN() { return _serializedATN; }
+
+  @Override
+  public ATN getATN() { return _ATN; }
+
+
+      // This method makes the parser stop running if it encounters
+      // invalid input and throw a RuntimeException.
+      public void reportErrorsAsExceptions() {
+          // To prevent any reports to standard error, add this line:
+          //removeErrorListeners();
+          
+          addErrorListener(new BaseErrorListener() {
+              public void syntaxError(Recognizer<?, ?> recognizer,
+                                      Object offendingSymbol, 
+                                      int line, int charPositionInLine,
+                                      String msg, RecognitionException e) {
+                  throw new ParseCancellationException(msg, e);
+              }
+          });
+      }
+
+  public ABCmusicParser(TokenStream input) {
+    super(input);
+    _interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+  }
+  public static class Abc_tuneContext extends ParserRuleContext {
+    public TerminalNode EOF() { return getToken(ABCmusicParser.EOF, 0); }
+    public Abc_tuneContext(ParserRuleContext parent, int invokingState) {
+      super(parent, invokingState);
+    }
+    @Override public int getRuleIndex() { return RULE_abc_tune; }
+    @Override
+    public void enterRule(ParseTreeListener listener) {
+      if ( listener instanceof ABCmusicListener ) ((ABCmusicListener)listener).enterAbc_tune(this);
+    }
+    @Override
+    public void exitRule(ParseTreeListener listener) {
+      if ( listener instanceof ABCmusicListener ) ((ABCmusicListener)listener).exitAbc_tune(this);
+    }
+  }
+
+  public final Abc_tuneContext abc_tune() throws RecognitionException {
+    Abc_tuneContext _localctx = new Abc_tuneContext(_ctx, getState());
+    enterRule(_localctx, 0, RULE_abc_tune);
+    try {
+      enterOuterAlt(_localctx, 1);
+      {
+      setState(2);
+      match(T__0);
+      setState(3);
+      match(EOF);
+      }
+    }
+    catch (RecognitionException re) {
+      _localctx.exception = re;
+      _errHandler.reportError(this, re);
+      _errHandler.recover(this, re);
+    }
+    finally {
+      exitRule();
+    }
+    return _localctx;
+  }
+
+  public static final String _serializedATN =
+    "\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\6\b\4\2\t\2\3\2"+
+      "\3\2\3\2\3\2\2\2\3\2\2\2\6\2\4\3\2\2\2\4\5\7\3\2\2\5\6\7\2\2\3\6\3"+
+      "\3\2\2\2\2";
+  public static final ATN _ATN =
+    new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+  static {
+    _decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+    for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+      _decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+    }
+  }
+}
